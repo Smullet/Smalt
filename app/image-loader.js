eaf562be1179e8646a8acacecba9d0ea77958108
Smullet <salome.mullet@orange.fr>
2025-06-03 13:43:11 +0200

    fix: Simplification de la configuration pour le déploiement

--- app/image-loader.js
+++
@@ -1,4 +0,0 @@
-export default function customImageLoader({ src, width, quality }) {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
-  return `${basePath}${src}?w=${width}&q=${quality || 75}`
-} 
--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, path: false };
-    return config;
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `/:path*`,
-      },
-    ]
-  },
+    unoptimized: true
+  }
 }
 
 export default nextConfig
